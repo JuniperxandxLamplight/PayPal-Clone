c4aed6c646a9a3111a1b22216909e702d2c038ca
Aurora Shido-Wagenet <fullmetalwoman@gmail.com>
2019-04-05 09:55:12 -0700

    add firebase service to inject navs

---
+++ src/app/firebase.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+@Injectable()
+export class FirebaseService {
+  navs: FirebaseListObservable<any[]>;
+
+  constructor(private database: AngularFireDatabase) {
+    this.navs = database.list('navigation');
+  }
+
+  getNavs(){
+    return this.navs;
+  }
+
+}
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-menu',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
+  navs: FirebaseListObservable<any[]>;
 
   constructor() { }
 
